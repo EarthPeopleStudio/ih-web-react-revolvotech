0ba7be9e2e6181bcede16875c9072c345fd2c67a
rajaharris55 <rajaharris55@gmail.com>
2025-03-28 15:54:30 +0500

    toml

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   server: {
     headers: {
       "Content-Security-Policy":
@@ -11,6 +12,9 @@ export default defineConfig({
     },
   },
   build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
     rollupOptions: {
       output: {
         manualChunks: undefined,
